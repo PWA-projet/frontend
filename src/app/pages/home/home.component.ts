3bbd9f89411274b3d70255c783424962cdd38a80
thibaut009 <gabet.thibaut@gmail.com>
2025-02-09 12:29:13 +0100

    Update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { Card } from 'primeng/card';
 import { Button } from 'primeng/button';
-import {NgForOf} from '@angular/common';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
     Card,
     Button,
-    NgForOf
+    NgForOf,
   ],
   templateUrl: './home.component.html',
   standalone: true,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  visible: boolean = false;
+
   channels = [
     { name: 'CGN 11/11' },
     { name: 'CGN 12/11' },
