d1dac2166a8496b0bcc229311afadad16ce82848
thibaut009 <gabet.thibaut@gmail.com>
2025-02-09 11:58:42 +0100

    Update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { Card } from 'primeng/card';
+import { Button } from 'primeng/button';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
-    Card
+    Card,
+    Button,
+    NgForOf
   ],
   templateUrl: './home.component.html',
   standalone: true,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  channels = [
+    { name: 'CGN 11/11' },
+    { name: 'CGN 12/11' },
+    { name: 'CGN 13/11' },
+    { name: 'CGN 14/11' },
+    { name: 'CGN 15/11' }
+  ];
 }
