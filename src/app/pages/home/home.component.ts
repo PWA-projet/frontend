648dcaf9cdaaf7b4d09e095292ad6e0a7815d728
thibaut009 <gabet.thibaut@gmail.com>
2025-02-09 13:20:02 +0100

    Update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { Card } from 'primeng/card';
-import { Button } from 'primeng/button';
-import { NgForOf } from '@angular/common';
+import { NgClass, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
     Card,
-    Button,
     NgForOf,
+    NgClass,
   ],
   templateUrl: './home.component.html',
   standalone: true,
@@ -22,6 +21,41 @@ export class HomeComponent {
     { name: 'CGN 12/11' },
     { name: 'CGN 13/11' },
     { name: 'CGN 14/11' },
-    { name: 'CGN 15/11' }
+    { name: 'CGN 15/11' },
+    { name: 'CGN 15/11' },
+    { name: 'CGN 15/11' },
+    { name: 'CGN 15/11' },
   ];
+
+  showDrawer() {
+    this.visible = !this.visible;
+    this.toggleOverlay();
+  }
+
+  // Ajouter une classe active à l'overlay pour assombrir l'écran
+  toggleOverlay() {
+    const overlay = document.querySelector('.page-overlay');
+    const body = document.body;
+
+    if (this.visible) {
+      overlay?.classList.add('active');
+      body.classList.add('page-no-interactions');
+    } else {
+      overlay?.classList.remove('active');
+      body.classList.remove('page-no-interactions');
+    }
+  }
+
+  // Ajouter un écouteur pour fermer le drawer si on clique en dehors du drawer
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const drawerElement = document.querySelector('.drawer');
+    const addContentElement = document.querySelector('.add-content');
+
+    // Si le clic est en dehors du drawer et du bouton "add content", on ferme le drawer
+    if (this.visible && !drawerElement?.contains(event.target as Node) && !addContentElement?.contains(event.target as Node)) {
+      this.visible = false;
+      this.toggleOverlay();
+    }
+  }
 }
