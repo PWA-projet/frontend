3b52c3fa02d2964cbc18e839fa995741b26c46e0
thibaut009 <gabet.thibaut@gmail.com>
2025-02-12 23:33:12 +0100

    Update channel

--- src/app/pages/channel/channel.component.ts
+++ src/app/pages/channel/channel.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChannelI } from '../../../shared/models/channel.model';
+import { ChannelService } from '../../../shared/services/channel.service';
+import { Card } from 'primeng/card';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-channel',
-  imports: [],
+  imports: [
+    Card,
+  ],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.css'
 })
-export class ChannelComponent {
+export class ChannelComponent implements OnInit {
+  channel!: ChannelI;
 
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private channelService: ChannelService,
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const channelId = params['id'];
+      this.loadChannel(channelId);
+    });
+  }
+
+  loadChannel(channelId: number) {
+    this.channelService.show(channelId).subscribe({
+      next: (data: ChannelI) => {
+        this.channel = data;
+        console.log(this.channel);
+      },
+      error: (error: any) => {
+        console.error('Error fetching channel:', error);
+      }
+    });
+  }
+
+  goToBack(): void {
+    this.location.back();
+  }
 }
