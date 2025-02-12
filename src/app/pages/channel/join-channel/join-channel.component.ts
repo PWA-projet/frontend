7df6bb3f644a4ef4547547911e81139e38054c5c
thibaut009 <gabet.thibaut@gmail.com>
2025-02-12 18:05:12 +0100

    Update

--- src/app/pages/channel/join-channel/join-channel.component.ts
+++ src/app/pages/channel/join-channel/join-channel.component.ts
@@ -2,24 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { InputText } from 'primeng/inputtext';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Button } from 'primeng/button';
-import { Location } from '@angular/common';
+import { Location, NgIf } from '@angular/common';
 import { ChannelService } from '../../../../shared/services/channel.service';
 import { APP_ROUTES } from '../../../../shared/constants/routes';
 import { Router } from '@angular/router';
+import { Message } from 'primeng/message';
 
 @Component({
   selector: 'app-join-channel',
-    imports: [
-        InputText,
-        FormsModule,
-        Button,
-        ReactiveFormsModule
-    ],
+  imports: [
+    InputText,
+    FormsModule,
+    Button,
+    ReactiveFormsModule,
+    Message,
+    NgIf
+  ],
   templateUrl: './join-channel.component.html',
   styleUrl: './join-channel.component.css'
 })
 export class JoinChannelComponent implements OnInit {
   joinChannelForm!: FormGroup;
+  channelNotFound: boolean = false;
 
   constructor(
     private fb: FormBuilder,
@@ -38,6 +42,7 @@ export class JoinChannelComponent implements OnInit {
       if (value) {
         this.joinChannelForm.patchValue({ key: value.toUpperCase() }, { emitEvent: false });
       }
+      this.channelNotFound = false; // Réinitialise l'erreur lors de la saisie
     });
   }
 
@@ -50,7 +55,10 @@ export class JoinChannelComponent implements OnInit {
           console.log(response.success);
           this.goToHome();
         },
-        error: (error) => console.error('Channel join failed:', error)
+        error: (error) => {
+          console.error('Channel join failed:', error);
+          this.channelNotFound = true; // Active le message d'erreur si la clé est incorrecte
+        }
       });
     } else {
       console.error('Form is invalid');
