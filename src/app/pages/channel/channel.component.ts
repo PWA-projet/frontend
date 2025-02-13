46e4db51992a1796d1713b766dc70cbb6cbcb87c
thibaut009 <gabet.thibaut@gmail.com>
2025-02-13 11:10:28 +0100

    Add message to channel

--- src/app/pages/channel/channel.component.ts
+++ src/app/pages/channel/channel.component.ts
@@ -3,18 +3,27 @@ import { ChannelI } from '../../../shared/models/channel.model';
 import { ChannelService } from '../../../shared/services/channel.service';
 import { Card } from 'primeng/card';
 import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+import {Location, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-channel',
   imports: [
     Card,
+    NgIf,
+    NgForOf,
   ],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.css'
 })
 export class ChannelComponent implements OnInit {
   channel!: ChannelI;
+  messages = [
+    { name: 'Alice', text: 'Bonjour tout le monde!' },
+    { name: 'Bob', text: 'Comment ça va ?' },
+    { name: 'Charlie', text: 'Ceci est un message test.' },
+    { name: 'David', text: 'Long message qui ne doit pas dépasser de la carte. Long message qui ne doit pas dépasser de la carte.' },
+    { name: 'Eve', text: 'Un autre message pour voir l’affichage.' }
+  ];
 
   constructor(
     private route: ActivatedRoute,
@@ -33,7 +42,6 @@ export class ChannelComponent implements OnInit {
     this.channelService.show(channelId).subscribe({
       next: (data: ChannelI) => {
         this.channel = data;
-        console.log(this.channel);
       },
       error: (error: any) => {
         console.error('Error fetching channel:', error);
--- src/app/pages/channel/create-channel/create-channel.component.ts
+++ src/app/pages/channel/create-channel/create-channel.component.ts
@@ -33,7 +33,7 @@ export class CreateChannelComponent implements OnInit {
 
   ngOnInit(): void {
     this.createChannelForm = this.fb.group({
-        name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]],
+        name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
       },
     );
   }
