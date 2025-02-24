999e403076046d89a28b1efcb42f5b047978207e
Baptiste <batbox.longuepee@gmail.com>
2025-02-24 14:25:36 +0100

    Update environment.prod.ts

--- src/shared/environments/environment.prod.ts
+++ src/shared/environments/environment.prod.ts
@@ -1,9 +1,7 @@
-let Host = 'http://127.0.0.1';
-let Port = '3333';
+let Host = 'https://keychat-api.thibaut009.com';
 
 export const environment = {
   production: true,
   apiHost: Host,
-  apiPort: Port,
-  apiUrl: `${Host}:${Port}`,
+  apiUrl: `${Host}`,
 };
