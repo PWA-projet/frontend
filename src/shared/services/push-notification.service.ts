fb2cd5d251f4bf33f937d579d73a9edf409dee66
Thibaut009 <gabet.thibaut@gmail.com>
2025-03-16 19:37:17 +0100

    Add notification push

---
+++ src/shared/services/push-notification.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment.dev';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PushNotificationService {
+  private apiUrl = `${environment.apiUrl}/channel`;
+  readonly VAPID_PUBLIC_KEY = 'BLSc8h04RqXb20KhjXaWMTFTL3hD0NXZ8r19TUWvt_NIkGo4zrEdhw4vUw2ZxraXOxNfJmoi8rrU4bHfFJD3H_s'; // Remplace avec ta clé publique
+
+  constructor(private swPush: SwPush, private http: HttpClient) {}
+
+  subscribeToPushNotifications() {
+    if (!this.swPush.isEnabled) {
+      console.log('Les notifications ne sont pas supportées');
+      return;
+    }
+
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    }).then(subscription => {
+      console.log('Subscription:', subscription);
+      // Envoie la souscription au backend
+      this.http.post<any>(`${this.apiUrl}/subscribe`, subscription).subscribe();
+    }).catch(err => console.error('Erreur de souscription', err));
+  }
+}
