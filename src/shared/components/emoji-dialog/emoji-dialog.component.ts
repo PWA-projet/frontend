428a77a08d783b22d6324680dd8e4ea2b7b0e1b8
thibaut009 <gabet.thibaut@gmail.com>
2025-04-03 17:47:35 +0200

    Update

--- src/shared/components/emoji-dialog/emoji-dialog.component.ts
+++ src/shared/components/emoji-dialog/emoji-dialog.component.ts
@@ -1,20 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { EmojiPickerComponent, EmojiSelectedEvent } from '@chit-chat/ngx-emoji-picker/lib/components/emoji-picker';
 import { ConnectedPosition } from '@angular/cdk/overlay';
 import { DialogComponent } from '@chit-chat/ngx-emoji-picker/lib/components/dialog';
 import { Button } from 'primeng/button';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-emoji-dialog',
   standalone: true,
-  imports: [DialogModule, EmojiPickerComponent, DialogComponent, Button],
+  imports: [DialogModule, EmojiPickerComponent, DialogComponent, Button, NgIf],
   templateUrl: './emoji-dialog.component.html',
   styleUrls: ['./emoji-dialog.component.css'],
 })
 export class EmojiDialogComponent {
   @Input() onEmojiSelect!: (emoji: string) => void;
   @Input() visible: boolean = false;
+  @ViewChild('emojiButton', { read: ElementRef }) emojiButton!: ElementRef;
+
+  isSmallScreen: boolean = window.innerWidth <= 700;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.isSmallScreen = window.innerWidth <= 700;
+  }
 
   dialogPositions: ConnectedPosition[] = [
     {
@@ -22,7 +30,7 @@ export class EmojiDialogComponent {
       originY: 'top',
       overlayX: 'end',
       overlayY: 'bottom',
-      offsetY: -10,
+      offsetY: -30,
     },
   ];
 
@@ -34,5 +42,9 @@ export class EmojiDialogComponent {
 
   toggleEmojiDialog() {
     this.visible = !this.visible;
+  }
+
+  get emojiButtonElement() {
+    return this.emojiButton ? this.emojiButton.nativeElement : null;
   }
 }
